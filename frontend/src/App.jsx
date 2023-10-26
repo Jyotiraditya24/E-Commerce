0c0deee40328d45cf544ca7ea8bc9412694d8f97
jyotiraditya <jyotiraditya24@gmail.com>
2023-10-26 19:10:44 +0530

    ProductList in making

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import ProductList from "./features/productList/productList";
 
 function App() {
-  return <div className="bg-red-200 ">Hello</div>;
+  return (
+    <div className="bg-red-400 ">
+      <ProductList></ProductList>
+    </div>
+  );
 }
 
 export default App;
