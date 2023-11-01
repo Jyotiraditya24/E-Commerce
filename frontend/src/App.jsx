bf684fcc7203ca3c359c009237e6d6e2173d2d02
jyotiraditya <jyotiraditya24@gmail.com>
2023-11-01 13:17:48 +0530

    LOGIN SIGN UP DONE

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,28 @@
 import "./App.css";
 import Home from "./pages/Home";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LoginPage from "./features/auth/components/LoginPage";
+import SignupPage from "./features/auth/components/SignupPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home></Home>,
+  },
+  {
+    path: "/login",
+    element: <LoginPage></LoginPage>,
+  },
+  {
+    path: "/signup",
+    element: <SignupPage></SignupPage>,
+  },
+]);
 
 function App() {
-  
   return (
     <div>
-      <Home></Home>
+      <RouterProvider router={router} />
     </div>
   );
 }
